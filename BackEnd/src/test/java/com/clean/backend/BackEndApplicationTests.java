b309f70a408171ec508e3cfdd268ffc6de332b9a
clean <2238650780@qq.com>
2023-11-18 23:24:43 +0800

    2023/11/18 11:24 Dao层、实体类简单示例 连接数据库 Dao层测试成功

--- BackEnd/src/test/java/com/clean/backend/BackEndApplicationTests.java
+++ BackEnd/src/test/java/com/clean/backend/BackEndApplicationTests.java
@@ -1,13 +1,19 @@
 package com.clean.backend;
 
+import com.clean.backend.entity.Visitor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import com.clean.backend.dao.VisitorDao;
 @SpringBootTest
 class BackEndApplicationTests {
 
+    @Autowired
+    VisitorDao visitorDao;
     @Test
     void contextLoads() {
+        Visitor visitor = visitorDao.getVisitorById(1);
+        System.out.println(visitor.toString());
     }
 
 }
--- README.md
+++ README.md
@@ -10,5 +10,11 @@
 2023/11/10 搭建项目基本结构
 
 ## 遇到的问题：
+2023/11/10  
 问题一： 多模块项目中，maven项目没有正确的被IDEA识别  
 问题分析：创建模块的方式有问题，删除之后在structure中重新创建项目
+  
+2023/11/18  
+问题二： 对Dao层进行测试的时候报出UnsatisfiedDependencyException和NoSuchBeanException  
+问题分析：初步判断是对应实体类的问题，进行排查之后发现问题不在这里， 定位到问题处在@Mapper注解上，  
+打开pom文件修改mybatis的配置文件后 问题消失，原因在于idea自动导入的mybatis不是经过springboot整合的。
